simahawk/edi

[IMP] 1st pre-commit execution

edi_voxel_account_invoice/models/res_config_settings.py
@@ -5,7 +5,8 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     voxel_invoice_login_id = fields.Many2one(
-        related='company_id.voxel_invoice_login_id', readonly=False)
+        related="company_id.voxel_invoice_login_id", readonly=False
+    )

OCA/edi-voxel

edi_voxel_oca/models/res_config_settings.py
@@ -5,11 +5,12 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     voxel_send_mode = fields.Selection(
-        related='company_id.voxel_send_mode', readonly=False)
-    voxel_sent_time = fields.Float(
-        related='company_id.voxel_sent_time', readonly=False)
+        related="company_id.voxel_send_mode", readonly=False
+    )
+    voxel_sent_time = fields.Float(related="company_id.voxel_sent_time", readonly=False)
     voxel_delay_time = fields.Float(
-        related='company_id.voxel_delay_time', readonly=False)
+        related="company_id.voxel_delay_time", readonly=False
+    )